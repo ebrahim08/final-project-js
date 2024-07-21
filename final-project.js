bff1508c3a67c8b3371add78fb61f263993ea7be
Ebrahim Razayee <Ebrahim.razayee@dci-student.org>
2024-07-21 21:03:54 +0200

    final-project

--- final-project.js
+++ final-project.js
@@ -1,24 +1,14 @@
-const prompt = require("prompt-sync")();
-//
-
-// data type
-// prompt is variable name
-
-// the part that is in right site is stored of prompt varable
-// reuire () => to import the library
-
-//  In Node.js, require is a function used to import modules (libraries) into your script.
-// 'prompt-sync' is the name of the module (library) that we want to import.
-// When you call require('prompt-sync'), Node.js looks for a module named prompt-sync in its module system.
-
-// You can use prompt (the function returned by prompt-sync) to prompt the user for input from the command line. It reads user input and returns it as a string.
+const readline = require("readline-sync");
 
 let wins = 0;
 let losses = 0;
 let ties = 0;
 
+// This starts an infinite loop that will keep running until the player chooses to quit.
 while (true) {
-  const playerchoice = prompt("Enter rock,paper of scissors (or q to quit):");
+  const playerchoice = readline.question(
+    "Enter rock, paper, or scissors (or q to quit): "
+  ); //prompt displays the message and waits for the player to type their choice.
   if (playerchoice.toLowerCase() === "q") {
     break;
   }
